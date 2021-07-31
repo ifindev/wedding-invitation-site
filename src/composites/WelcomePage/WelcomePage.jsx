8c3af209d59383b6b9312ef5e6db74278fdc56bd
Muhammad Arifin <muhammad.arifin1@ibm.com>
2021-07-31 18:47:13 +0700

    Finished responsive welcome page

--- src/composites/WelcomePage/WelcomePage.jsx
+++ src/composites/WelcomePage/WelcomePage.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { HeaderSeparator, Button } from '../../components'
+
 import styles from './styles.module.css'
 
 const WelcomePage = () => {
-  return <div className={styles.container}>Hello</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.separatorContainer}>
+        <HeaderSeparator />
+        <p className={styles.textName}>Arifin &amp; Fitria</p>
+        <HeaderSeparator />
+      </div>
+      <p className={styles.invitationText}>
+        We are getting married, and we want you <br /> to be part of our special
+        day
+      </p>
+      <p className={styles.invitationDate}>Saturday, 27th November 2021</p>
+      <div className={styles.buttonContainer}>
+        <Button text="Save the Date!" />
+      </div>
+    </div>
+  )
 }
 
 export default WelcomePage
