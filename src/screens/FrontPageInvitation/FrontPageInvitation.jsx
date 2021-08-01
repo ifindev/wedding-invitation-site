5a5cee4b2a83e0f8555f683b3fb11d0d776ef0d6
Muhammad Arifin <muhammad.arifin1@ibm.com>
2021-08-01 10:41:12 +0700

    Fix mobile view of frontpageInvitation

--- src/screens/FrontPageInvitation/FrontPageInvitation.jsx
+++ src/screens/FrontPageInvitation/FrontPageInvitation.jsx
@@ -5,22 +5,26 @@ import styles from './styles.module.css'
 
 const FrontPageInvitation = ({ setInvitationOpen }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.separatorContainer}>
-        <HeaderSeparator />
-        <p className={styles.textName}>Arifin &amp; Fitria</p>
-        <HeaderSeparator />
+    <div>
+      <div className={styles.mobileAdditionalBg} />
+      <div className={styles.container}>
+        <div className={styles.separatorContainer}>
+          <HeaderSeparator />
+          <p className={styles.textName}>Arifin &amp; Fitria</p>
+          <HeaderSeparator />
+        </div>
+        <p className={styles.invitationText}>
+          We are getting married, and we want you <br /> to be part of our
+          special day
+        </p>
+        <div className={styles.buttonContainer}>
+          <Button
+            text="Open Invitation"
+            onClick={() => setInvitationOpen(true)}
+          />
+        </div>
       </div>
-      <p className={styles.invitationText}>
-        We are getting married, and we want you <br /> to be part of our special
-        day
-      </p>
-      <div className={styles.buttonContainer}>
-        <Button
-          text="Open Invitation"
-          onClick={() => setInvitationOpen(true)}
-        />
-      </div>
+      <div className={styles.mobileAdditionalBg} />
     </div>
   )
 }
