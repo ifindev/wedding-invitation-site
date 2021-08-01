875228767e412e9826ebff5772ec1052e181df79
Muhammad Arifin <muhammad.arifin1@ibm.com>
2021-08-01 09:44:15 +0700

    Added venue map

---
+++ src/components/GoogleMapLocation/GoogleMapLocation.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet'
+import styles from './styles.module.css'
+
+const GoogleMapLocation = () => {
+  return (
+    <>
+      <MapContainer
+        className={styles.mapLocation}
+        center={[-6.188749993366775, 106.87747132606388]}
+        zoom={30}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-6.188749993366775, 106.87747132606388]}>
+          <Tooltip direction="top" offset={[0, -10]} opacity={1} permanent>
+            Masjid Salman Al-Farisi
+          </Tooltip>
+        </Marker>
+      </MapContainer>
+    </>
+  )
+}
+
+export default GoogleMapLocation
--- src/composites/EventDetails/EventDetails.jsx
+++ src/composites/EventDetails/EventDetails.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { EventDetailCard } from '../../components'
+import { Button, EventDetailCard, GoogleMapLocation } from '../../components'
 import styles from './styles.module.css'
 
 const EventDetails = () => {
+  const venueUrl =
+    'https://www.google.com/maps/place/Salman+Al-Farisi+Mosque/@-6.188782,106.875347,17z/data=!3m1!4b1!4m5!3m4!1s0x2e69f4ee174c6c43:0xa8bb47340df92921!8m2!3d-6.1888751!4d106.8774866'
   const eventDetails = [
     {
       name: 'Akad Nikah',
@@ -28,7 +30,7 @@ const EventDetails = () => {
         <p className={styles.youThere}>WE WANT YOU THERE!</p>
         <p className={styles.joinUs}>Join our happy moment</p>
         <div className={styles.eventContainer}>
-          {eventDetails.map((item) => {
+          {eventDetails.map((item, idx) => {
             return (
               <EventDetailCard
                 eventName={item.name}
@@ -36,10 +38,17 @@ const EventDetails = () => {
                 eventTime={item.time}
                 eventVenue={item.venue}
                 venueAddress={item.address}
+                key={idx}
               />
             )
           })}
         </div>
+      </div>
+      <div className={styles.location}>
+        <div>
+          <GoogleMapLocation />
+        </div>
+        <Button text="Lihat lokasi" onClick={() => window.open(venueUrl)} />
       </div>
     </div>
   )
