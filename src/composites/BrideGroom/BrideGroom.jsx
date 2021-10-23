82c57c36170936ef94260749980512196544bdec
Muhammad Arifin <muhammad.arifin1@ibm.com>
2021-10-23 10:13:49 +0700

    Refactor BrideGroom section

--- src/composites/BrideGroom/BrideGroom.jsx
+++ src/composites/BrideGroom/BrideGroom.jsx
@@ -4,29 +4,25 @@ import { ProfileCard } from '../../components'
 
 // Assets
 import styles from './styles.module.css'
-import bride from '../../assets/2_bride_img.png'
-import groom from '../../assets/3_groom.png'
+import { data } from './data'
 
 const BrideGroom = () => {
   return (
     <div className={styles.container}>
       <p className={styles.title}>The Bride &amp; The Groom</p>
       <div className={styles.cardContainer}>
-        <ProfileCard
-          img={bride}
-          title="The Bride"
-          name="Fitria Andaryani, S.Si., FSAI"
-          description="Putri Ketiga"
-          parent="Bapak Suhartono &amp; Ibu Komariyah"
-        />
-        <div className={styles.separator} />
-        <ProfileCard
-          img={groom}
-          title="The Groom"
-          name="Muhammad Arifin, S.T."
-          description="Putra Pertama"
-          parent={'Alm. Bapak Dr. Ing. R. Riesdam Effendi & Ibu Tjut Mutiah'}
-        />
+        {data.map((el, idx) => (
+          <React.Fragment>
+            <ProfileCard
+              img={el.img}
+              title={el.title}
+              name={el.name}
+              description={el.description}
+              parent={el.parent}
+            />
+            {(idx + 1) % 2 !== 0 ? <div className={styles.separator} /> : null}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   )
---
+++ src/composites/BrideGroom/data.js
@@ -0,0 +1,19 @@
+import bride from '../../assets/2_bride_img.png'
+import groom from '../../assets/3_groom.png'
+
+export const data = [
+  {
+    img: bride,
+    title: 'The Bride',
+    name: 'Fitria Andaryani, S.Si., FSAI',
+    description: 'Putri Ketiga',
+    parent: 'Bapak Suhartono &amp; Ibu Komariyah',
+  },
+  {
+    img: groom,
+    title: 'The Groom',
+    name: 'Muhammad Arifin, S.T.',
+    description: 'Putra Pertama',
+    parent: 'Alm. Bapak Dr. Ing. R. Riesdam Effendi &amp; Ibu Tjut Mutiah',
+  },
+]
